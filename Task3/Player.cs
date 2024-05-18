321a5a20dbe9a4002fc57c4f84c0bc96d5340974
Yana Makogon <yana.mak89@gmail.com>
2024-05-18 20:54:46 +0200

    Task 3 updated

--- Task3/Player.cs
+++ Task3/Player.cs
@@ -1,6 +1,23 @@
 namespace Task3;
 
-public class Player
+public class Player : IPlayable, IRecodable
 {
+    void IPlayable.Play()
+    {
+        Console.WriteLine("Playing ... ");
+    }
+    
+    void IRecodable.Record()
+    {
+        Console.WriteLine("Recording ... ");
+    }
+    public void Pause()
+    {
+        Console.WriteLine("Pausing ... ");
+    }
+    public void Stop()
+    {
+        Console.WriteLine("Stoping ... ");
+    }
     
 }
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Task3;
+
+class Program
+{
+    static void Main(string[] arg)
+    {
+        Player player = new Player();
+        
+        Console.WriteLine("Playback Operations:");
+        ((IPlayable)player).Play();
+        player.Pause();
+        player.Stop();
+        
+        Console.WriteLine("\nRecording Operations:");
+        ((IRecodable)player).Record();
+        player.Pause();
+        player.Stop();
+    }
+}
